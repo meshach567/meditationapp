c2fa376a89cb62af3c914d4da15fc637788e415f
meshach567 <arinzemeshach123@gmail.com>
2024-10-22 12:09:15 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
